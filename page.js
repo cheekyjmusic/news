f5e330dafaa6a85e66c7ca754ea20cf10df5ec66
cheekyj <69768429+cheekyjmusic@users.noreply.github.com>
2023-10-25 20:14:38 -0700

    added 102623

---
+++ page.js
@@ -0,0 +1,29 @@
+function fullscreen() {
+    if (!document.fullscreenElement) {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+function goToNews(article){
+    window.location.href = 'https://cheekyjmusic.github.io/news/read?article=' + article;
+}
+function redirect(url){
+    window.location.href = url;
+}
